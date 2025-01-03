75c64c85269ad08635d830e416fa1ff8571337b6
Nils Ramolla <nilsramolla@me.com>
2025-01-03 11:39:56 +0100

    WIP

--- main.py
+++ main.py
@@ -3,14 +3,10 @@ from packages.visualization.plotly import histogram
 
 path_for_images = "./data/Unruhig_Format16-9"
 
-pictures, picture_names = fe.get_images_and_convert_to_grayscale(path_for_images)
+pictures = fe.get_images_and_convert_to_grayscale(path_for_images)
 
-data = []
+print(len(pictures))
 
-for picture, picture_name in zip(pictures, picture_names):
-    contrast = fe.get_contrast(picture)
-    data.append(contrast)
 
-histogram(data)
 
 
--- packages/visualization/plotly.py
+++ packages/visualization/plotly.py
@@ -1,57 +1,18 @@
 import math
-from typing import List
+from typing import List, Tuple
 
 import numpy as np
 import plotly.graph_objects as go
 from plotly.graph_objs import Figure
 
 
-def histogram(data: List[float | np.floating], title:str = "Default Title", x_label:str="X-Axis", y_label:str="Y-Axis") -> Figure:
+def histogram(data: List[Tuple[np.floating, str]], title:str = "Default Title", x_label:str="X-Axis", y_label:str="Y-Axis"):
 
     num_bins = 16
 
-    #Increasing the upper border by an epsilon to include the last datapoint in the last bin
-    bins = np.linspace(start=min(data), stop=max(data) + 1e-5, num=num_bins)
+    bin_size = (max(data) - min(data)) / num_bins
 
-    bin_indices = np.digitize(data, bins)
-
-    bin_counts = [bin_indices.tolist().count(i) for i in range(1,len(bins))]
-
-    if sum(bin_counts) < len(data):
-        raise Exception("Not all datapoints where distributed in bins.")
-
-    bin_elements = {i:[] for i in range(len(bins))}
-
-    for d,b in zip(data, bin_indices):
-        bin_elements[b].append(d)
-
-    # Texte für Hover-Info erstellen
-    bin_hover_text = [
-        f"Bin {i}: {bin_elements[i]}" for i in range(1, len(bins))
-    ]
-
-    min_value = math.floor(min(data) / 10) * 10
-    max_value = math.ceil(max(data)/10) * 10
-
-
-
-    print(min_value, max_value)
-    # Plotly-Histogramm erstellen
-    fig = go.Figure(go.Bar(
-        y=bin_counts,  # Häufigkeiten
-        text=bin_hover_text,  # Hover-Text
-        hoverinfo="x+y+text"  # Zeige x-Wert, y-Wert und Text
-    ))
-
-    fig.update_layout(
-        title="Histogram - Contrast",
-        yaxis_title="Number of Pictures",
-        xaxis=dict(
-            title="Contrast",
-            tickvals=np.linspace(min_value, max_value, num=10),
-            ticktext=[f"{int(val)}" for val in np.linspace(min_value, max_value, num=10)],
-        )
-    )
-
+    fig = go.Figure(go.Histogram(x=data, xbins=dict(start=min(data), end=max(data), size=bin_size),hoverinfo="x+y" ))
+    fig.update_layout(bargap=0.2, title=title, xaxis_title=x_label, yaxis_title=y_label)
     fig.show()
 
