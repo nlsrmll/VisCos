4cbb0786171109cf765427ccd0b9059cc623fd4d
Nils Ramolla <nilsramolla@me.com>
2025-01-17 11:45:52 +0100

    Refined the plotting for a better modularity

--- cluster_test.py
+++ cluster_test.py
@@ -1,5 +1,14 @@
+from packages.clustering.knn import ClusterAnalyze
 from packages.image_analyse.DataReader import CSVDataFrame
 
 path = "./data/likert_results.csv"
 
-likert_data = CSVDataFrame(path)
+likert_data = CSVDataFrame(path, True)
+likert_data.remove_row("912858", "30117")
+
+cluster_analyzer = ClusterAnalyze(likert_data)
+cluster_analyzer.seed = 42
+
+cluster_analyzer.visualize_knn(cluster_count=3)
+
+cluster_analyzer.boxplot_all_clusters()
--- packages/visualization/plotly.py
+++ packages/visualization/plotly.py
@@ -1,4 +1,6 @@
 import datetime
+import numbers
+from typing import List
 
 import numpy as np
 import pandas
@@ -11,6 +13,26 @@ def histogram(
     x_label: str = "X-Axis",
     y_label: str = "Y-Axis",
 ):
+    """
+    Creates and displays an interactive histogram using Plotly, with hoverable information for each bin.
+
+    Parameters:
+        df (pandas.DataFrame): A DataFrame with two columns:
+            - The first column contains names (used as hover text for bins).
+            - The second column contains numerical data used to generate the histogram.
+        title (str, optional): Title of the histogram. Defaults to "Default Title".
+        x_label (str, optional): Label for the x-axis. Defaults to "X-Axis".
+        y_label (str, optional): Label for the y-axis. Defaults to "Y-Axis".
+
+    Raises:
+        AssertionError: If some data points are not assigned to any bin.
+
+    Notes:
+        - The number of bins is fixed at 16.
+        - Bin edges are calculated dynamically based on the range of the data.
+        - Hover text shows the names of elements within each bin.
+        - An annotation with the total number of elements and creation timestamp is added below the plot.
+    """
 
     data = df.iloc[:, 1]
     names = df.iloc[:, 0]
@@ -74,3 +96,97 @@ def histogram(
     )
 
     fig.show()
+
+
+def scatter(
+    x: List[numbers.Number],
+    y: List[numbers.Number],
+    title: str = "Default Title",
+    x_label: str = "X-Axis",
+    y_label: str = "Y-Axis",
+    **kwargs,
+):
+    """
+    Creates and displays a scatter plot using Plotly with customizable labels, title, and additional parameters.
+
+    Parameters:
+        x (List[numbers.Number]): A list of numerical values for the x-axis.
+        y (List[numbers.Number]): A list of numerical values for the y-axis.
+        title (str, optional): Title of the scatter plot. Defaults to "Default Title".
+        x_label (str, optional): Label for the x-axis. Defaults to "X-Axis".
+        y_label (str, optional): Label for the y-axis. Defaults to "Y-Axis".
+        **kwargs: Additional keyword arguments passed to `go.Scatter` for customization (e.g., marker size, color).
+
+    Notes:
+        - The function uses `base_fig` to create the figure layout and adds a `Scatter` trace for the data.
+        - The scatter plot is displayed immediately using `fig.show()`.
+        - An annotation with the creation timestamp is added below the plot.
+    """
+    fig = base_fig(
+        title=title,
+        x_label=x_label,
+        y_label=y_label,
+        annotation_text=f"Created at: {datetime.datetime.now().strftime('%d.%m.%Y %H:%M')} ",
+    )
+
+    fig.add_trace(go.Scatter(x=x, y=y, mode="markers", **kwargs))
+
+    fig.show()
+
+
+def base_fig(
+    title: str = "Default Title",
+    x_label: str = "X-Axis",
+    y_label: str = "Y-Axis",
+    annotation_text: str = "",
+) -> go.Figure:
+    """
+    Creates a base Plotly figure with a customizable title, axis labels, and annotation.
+
+    Parameters:
+        title (str, optional): Title of the figure. Defaults to "Default Title".
+        x_label (str, optional): Label for the x-axis. Defaults to "X-Axis".
+        y_label (str, optional): Label for the y-axis. Defaults to "Y-Axis".
+        annotation_text (str, optional): Text to annotate below the plot. Defaults to an empty string.
+
+    Returns:
+        go.Figure: A Plotly Figure object with the specified layout and annotation.
+
+    Notes:
+        - The title and axis labels are automatically capitalized.
+        - Annotations are positioned below the plot with no arrow and centered alignment.
+        - This function creates a base figure, which can be further customized or populated with data.
+    """
+
+    fig = go.Figure()
+
+    fig.update_layout(
+        title=dict(text=title.title(), xanchor="center", yanchor="top", x=0.5),
+        yaxis=dict(
+            title=y_label.title(),
+            ticklen=5,
+            tickwidth=1,
+            tickcolor="black",
+            ticks="outside",
+        ),
+        xaxis=dict(
+            title=x_label.title(),
+            ticklen=5,
+            title_standoff=2,
+            tickwidth=1,
+            tickcolor="black",
+            ticks="outside",
+        ),
+    )
+
+    fig.add_annotation(
+        text=annotation_text,
+        xref="paper",
+        yref="paper",
+        x=0.5,
+        y=-0.2,
+        showarrow=False,
+        align="center",
+    )
+
+    return fig
